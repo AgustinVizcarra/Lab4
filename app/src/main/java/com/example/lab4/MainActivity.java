e07b3b7b229015ad2916d137af5535c53b80b714
Ronny Pastor <a20185534@pucp.edu.pe>
2022-11-04 08:36:35 -0500

    Main activity and loginactivity

--- app/src/main/java/com/example/lab4/MainActivity.java
+++ app/src/main/java/com/example/lab4/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.lab4;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button botonAdmin = findViewById(R.id.button_admin);
+        botonAdmin.setOnClickListener(view -> {
+            Intent intent = new Intent(MainActivity.this, Admin.class);
+            startActivity(intent);
+        });
+        Button botonUsuario = findViewById(R.id.button_user);
+        botonUsuario.setOnClickListener(view -> {
+            Intent intent = new Intent(MainActivity.this,UsuarioActivity.class);
+            startActivity(intent);
+        });
+
     }
 }
