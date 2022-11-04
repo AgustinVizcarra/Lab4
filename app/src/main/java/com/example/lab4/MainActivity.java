ff8b60d41e7df69a6e768033bb2a83750158f699
Alonso Rosales <a20191566@pucp.edu.pe>
2022-11-04 16:12:42 -0500

    logeo terminado

--- app/src/main/java/com/example/lab4/MainActivity.java
+++ app/src/main/java/com/example/lab4/MainActivity.java
@@ -1,13 +1,26 @@
 package com.example.lab4;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
 import android.widget.Button;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class MainActivity extends AppCompatActivity {
+    private GoogleSignInClient mGoogleSignInClient;
+    private GoogleSignInOptions gso;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +36,36 @@ public class MainActivity extends AppCompatActivity {
             Intent intent = new Intent(MainActivity.this,UsuarioActivity.class);
             startActivity(intent);
         });
+
+        gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
+        mGoogleSignInClient =GoogleSignIn.getClient(this,gso);
+
+
+       Button botonSalir = findViewById(R.id.butonlogouat);
+       botonSalir.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View view) {
+
+               FirebaseAuth.getInstance().signOut();
+
+               mGoogleSignInClient.signOut().addOnCompleteListener(new OnCompleteListener<Void>() {
+                   @Override
+                   public void onComplete(@NonNull Task<Void> task) {
+                       Intent intent = new Intent(getApplicationContext(),LoginActivity.class);
+                       startActivity(intent);
+                       MainActivity.this.finish();
+
+                   }
+               });
+
+           }
+       });
+
+
 
     }
 
